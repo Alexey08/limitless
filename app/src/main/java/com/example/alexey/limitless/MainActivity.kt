bd0a02228111c85803e1fad73c3b25e0c9fcc5ed
Alexey <bauer080@gmail.ru>
2019-05-10 23:24:39 +0500

    working version

--- app/src/main/java/com/example/alexey/limitless/MainActivity.kt
+++ app/src/main/java/com/example/alexey/limitless/MainActivity.kt
@@ -3,17 +3,70 @@ package com.example.alexey.limitless
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.main_layout.*
+import okhttp3.*
+import okhttp3.OkHttpClient
+import java.io.IOException
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_layout)
+
+//        tv_start.text = "new text"
+
         bt_ok.setOnClickListener {
             val myIntent = Intent(this, MenuActivity::class.java)
             startActivity(myIntent)
         }
+
+        mJson1()
+    }
+
+    private fun mJson1() {
+
+        val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
+        val request = Request.Builder().url(url).build()
+
+        val client = OkHttpClient()
+
+        client.newCall(request).enqueue(object: Callback {
+            override fun onResponse(call: Call, response: Response) {
+
+                val body = response.body()?.string()
+                println(body)
+
+                val gson = GsonBuilder().create()
+                println(gson)
+
+                val startFeed = gson.fromJson(body,StartFeed::class.java)
+
+                runOnUiThread {
+                    tv_start.text = startFeed.videos[0].name
+                }
+            }
+
+            override fun onFailure(call: Call?, e: IOException?) {
+                println("Error! Failed to execute request!")
+            }
+        })
     }
 }
+
+class StartFeed (val videos: List<Video>)
+
+class Video (val name: String)
+
+
+
+
+
+// val url = "https://api.vk.com/method/wall.get?domain=englimitless&count=10&access_token=06aeee6606aeee6606aeee665e06c4b097006ae06aeee665a6385d379edae733141a766&v=5.95"
+//
+// val url = "https://api.letsbuildthatapp.com/youtube/home_feed"
+
--- app/src/main/java/com/example/alexey/limitless/MenuAdapter.kt
+++ app/src/main/java/com/example/alexey/limitless/MenuAdapter.kt
@@ -1,13 +1,35 @@
 package com.example.alexey.limitless
 
-import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.menu_item.view.*
 
-class MenuAdapter(private val items : ArrayList<String>, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class MenuAdapter(val menuFeed: MenuFeed) : RecyclerView.Adapter<ViewHolder>() {
+
+//    val videoTitles = listOf("123")
+
+    override fun getItemCount(): Int {
+        return menuFeed.videos.count()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.menu_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val video = menuFeed.videos[position]
+        holder.view.tv_item.text = video.name
+    }
+}
+
+class ViewHolder (val view: View) : RecyclerView.ViewHolder(view) {
+//    val tvMenuItem = view.tv_item!!
+}
+
+/*
+class MenuAdapter(private val items: MenuFeed, private val context: Context) : RecyclerView.Adapter<ViewHolder>() {
 
     override fun getItemCount(): Int {
         return items.size
@@ -24,4 +46,4 @@ class MenuAdapter(private val items : ArrayList<String>, private val context: Co
 
 class ViewHolder (view: View) : RecyclerView.ViewHolder(view) {
     val tvMenuItem = view.tv_item!!
-}
+ */
